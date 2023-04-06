9572b1174d633556309465fae732de1938ff9a35
ltwmori <ltwmori@gmail.com>
2023-04-07 00:06:50 +0600

    fix: fixed footer text

--- src/layout/Footer/index.tsx
+++ src/layout/Footer/index.tsx
@@ -4,7 +4,9 @@ const Footer = () => {
   return (
     <footer className="flex items-end lg:pb-14 pl-14 pr-14 lg:pl-24 lg:pr-24 mt-auto h-24 lg:h-48 bg-footerGray font-nagelRegular">
       <div className="flex items-center justify-between w-full">
-        <p className="text-grey1 text-xs">Assel luchshaya!!</p>
+        <p className="text-grey1 text-xs">
+          © 2022 PINCODE. Все права защищены.
+        </p>
         <div>logo</div>
         <p>Designed by znat'</p>
       </div>
