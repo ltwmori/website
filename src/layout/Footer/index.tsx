376f13b310594a7f0012aed6428b06426cd0c74b
ltwmori <ltwmori@gmail.com>
2023-04-19 01:09:32 +0600

    fix: logo

--- src/layout/Footer/index.tsx
+++ src/layout/Footer/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
-
+import logo from '../../../assets/logo.png'
 const Footer = () => {
   return (
-    <footer className="flex items-end lg:pb-14 pl-14 pr-14 lg:pl-24 lg:pr-24 mt-auto h-24 lg:h-48 bg-footerGray font-nagelRegular">
-      <div className="flex items-center justify-between w-full">
+    <footer className="flex items-end lg:pb-14 pl-14 pr-14 lg:pl-24 lg:pr-24 mt-auto h-24 lg:h-48 bg-white font-nagelRegular">
+      <div className="flex items-end justify-between w-full">
         <p className="text-grey1 text-xs">
           © 2022 PINCODE. Все права защищены.
         </p>
-        <div>logo</div>
+        <div>
+          <img src={logo} className='object-contain w-2/3'></img>
+        </div>
         <p>Designed by znat'</p>
       </div>
     </footer>
